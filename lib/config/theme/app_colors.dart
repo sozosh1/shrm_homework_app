32adacdb5e8b5e30d61b1e2210ea8856a962ff36
sozosh1 <arbaevaa@mail.ru>
2025-06-12 18:04:20 +0300

    create app_colors class

---
+++ lib/config/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryGreen = Color(0xFF2AE881);
+  static const lightGreen = Color(0xFFD4FAE6);
+  static const whiteBackground = Color(0xFFFEF7FF);
+  static const lightBackground = Color(0xFFF3EDF7);
+  static const textDark = Color(0xFF49454F);
+}
