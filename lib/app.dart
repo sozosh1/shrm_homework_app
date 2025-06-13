1d4f59c8c5c64cb44c823fec157640b78987de7e
sozosh1 <arbaevaa@mail.ru>
2025-06-13 18:27:30 +0300

    add tabbar navigation

--- lib/app.dart
+++ lib/app.dart
@@ -1,68 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:shrm_homework_app/config/theme/app_colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'FINANCE app',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryGreen),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 1;
+  final PageController _pageController = PageController(initialPage: 1);
 
-  void _incrementCounter() {
+  void _onItemTapped(int index) {
     setState(() {
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        backgroundColor: AppColors.primaryGreen,
+        title: const Text(
+          'Ало бизнес, да да деньги',
+          style: TextStyle(color: Colors.white),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Placeholder(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/expense.svg',
+              width: 24,
+              height: 24,
+
+              colorFilter: ColorFilter.mode(
+                _selectedIndex == 0
+                    ? AppColors.primaryGreen
+                    : AppColors.textDark,
+                BlendMode.srcIn,
+              ),
+            ),
+
+            label: 'Расходы',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/income.svg',
+              width: 24,
+              height: 24,
+              colorFilter: ColorFilter.mode(
+                _selectedIndex == 1
+                    ? AppColors.primaryGreen
+                    : AppColors.textDark,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: 'Доходы',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/account.svg',
+              width: 24,
+              height: 24,
+              colorFilter: ColorFilter.mode(
+                _selectedIndex == 2
+                    ? AppColors.primaryGreen
+                    : AppColors.textDark,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: 'Счет',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/article.svg',
+              width: 24,
+              height: 24,
+              colorFilter: ColorFilter.mode(
+                _selectedIndex == 3
+                    ? AppColors.primaryGreen
+                    : AppColors.textDark,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: 'Статьи',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/settings.svg',
+              width: 24,
+              height: 24,
+              colorFilter: ColorFilter.mode(
+                _selectedIndex == 4
+                    ? AppColors.primaryGreen
+                    : AppColors.textDark,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: 'Настройки',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: AppColors.primaryGreen,
+        unselectedItemColor: AppColors.textDark,
+        backgroundColor: AppColors.whiteBackground,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        showUnselectedLabels: true,
+      ),
+      backgroundColor: AppColors.lightBackground,
     );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 }
