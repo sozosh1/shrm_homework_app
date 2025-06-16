19af18db96be08eb60f7a8311491c30c6f3c60ec
sozosh1 <arbaevaa@mail.ru>
2025-06-17 00:00:47 +0300

    delete useless name

--- lib/features/account/domain/repository/account_repository.dart
+++ lib/features/account/domain/repository/account_repository.dart
@@ -1,15 +1,15 @@
-import 'package:shrm_homework_app/features/account/data/models/account_create_request/account_create_request.dart'
-    as data_account;
-import 'package:shrm_homework_app/features/account/data/models/account_history_response/account_history_response.dart'
-    as data_account;
-import 'package:shrm_homework_app/features/account/data/models/account_update_request/account_update_request.dart'
-    as data_account;
+import 'package:shrm_homework_app/features/account/data/models/account_create_request/account_create_request.dart';
+
+import 'package:shrm_homework_app/features/account/data/models/account_history_response/account_history_response.dart';
+
+import 'package:shrm_homework_app/features/account/data/models/account_update_request/account_update_request.dart';
+
 import 'package:shrm_homework_app/features/account/domain/models/account/account.dart';
 
 abstract class AccountRepository {
-  Future<Account> createAccount(data_account.AccountCreateRequest request);
-  Future<Account> updateAccount(int id,data_account.AccountUpdateRequest request);
+  Future<Account> createAccount(AccountCreateRequest request);
+  Future<Account> updateAccount(int id, AccountUpdateRequest request);
   Future<Account> getAccount(int id);
   Future<void> deleteAccount(int id);
-  Future<data_account.AccountHistoryResponse> getAccountHistory(int id);
+  Future<AccountHistoryResponse> getAccountHistory(int id);
 }
